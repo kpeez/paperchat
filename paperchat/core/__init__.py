3d3cb0820a66d972dc259cf742f44b0013451ae0
kpuhger <krpuhger@gmail.com>
2026-03-07 22:32:23 -0800

    chore(repo): remove legacy prototype code

--- paperchat/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from .embeddings import get_embedding_model
-from .ingestion import process_pdf_document
-from .rag_pipeline import RAGPipeline
-from .vector_store import VectorStore
-
-__all__ = ["RAGPipeline", "VectorStore", "get_embedding_model", "process_pdf_document"]
